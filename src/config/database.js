7997045df88ffef44c4c65ac624f255d6abcf686
Ashish Raj <ashishrahul748@gmail.com>
2024-06-15 16:25:24 +0530

    checking auth

--- src/config/database.js
+++ src/config/database.js
@@ -1,16 +1,19 @@
-import mongoose from 'mongoose';
+import mongoose from "mongoose";
 
 export const connect = async () => {
-    try {
-        await mongoose.connect('mongodb://localhost:27017/med', {
-            useNewUrlParser: true,
-            useUnifiedTopology: true
-        });
-        console.log('MongoDB connected successfully');
-    } catch (error) {
-        console.error('MongoDB connection failed:', error.message);
-        process.exit(1); // Exit process with failure
-    }
+  try {
+    await mongoose.connect(
+      "mongodb+srv://auth:auth123@cluster0.cgqlp6h.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0",
+      {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+      }
+    );
+    console.log("MongoDB connected successfully");
+  } catch (error) {
+    console.error("MongoDB connection failed:", error.message);
+    process.exit(1); // Exit process with failure
+  }
 };
 
-// export default connect;
+export default connect;
--- src/index.js
+++ src/index.js
@@ -2,11 +2,13 @@ import express from 'express';
 import cors from 'cors';
 import bodyParser from 'body-parser';
 import {connect} from './config/database.js'
-import apiRoutes from './route/index.js'
+import apiRoutes from './routes/index.js'
 
 const app = express(); 
 
-app.use(cors());
+app.use(cors({
+    origin:"*"
+}));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}))
 app.use('/api',apiRoutes)
